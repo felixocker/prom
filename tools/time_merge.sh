56e89264be99b8d62b66fe05f08a9001c59d5e77
Felix Ocker <felix.ocker@googlemail.com>
2022-03-18 19:58:29 +0100

    feat: support timing for all matching tasks in benchmark

--- tools/time_merge.sh
+++ tools/time_merge.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # run and time main.py, return execution times in seconds separated by commas
-# NOTE: make sure to set up the config file correctly; possibly comment out abox merging
+# NOTE: make sure to set up the config file correctly; possibly comment out abox merging in main.py
+# NOTE: uncomment code below as needed for MEXT/ MEXO, MVSP, and OAEI examples; place respective ontos in tools/
 # Usage: bash time_merge.sh iterations
 
 iterations="$1"
@@ -21,12 +22,18 @@ TIMEFORMAT="%R"
 # timing
 for n in $(seq $iterations); do
 	echo "iteration: $n"
+  # MEXT/ MEXO; works with "config_min.yml", set min-example to False
+  cp ../tools/*.owl ../data/
+  # MVSP; works with "alt_config.yml"
+  # cp ../tools/manufacturing-capability ../tools/mason_reduced.owl ../data/
+  # OAEI; works with "config_YYY.yml"; set reject threshold to .6 for automatic mode
+  # cp ../tools/*.rdf ../data/
 	exec 3>&1 4>&2
 	TIMING=$( { time python main.py 1>&3 2>&4; } 2>&1 )
 	exec 3>&- 4>&-
 	echo "timing result for iteration $n: $TIMING"
 	RES+="${RES:+,}$TIMING"
-	rm ../data/*.owl
+	rm ../data/*.owl ../data/manufacturing-capability ../data/*.rdf
 done
 
 # reset
@@ -35,4 +42,3 @@ unset TIMEFORMAT
 
 # return results
 printf "overall timing results:\n$RES\n"
-
