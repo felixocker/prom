e1094480fae5814abb417724ca8d9c39cc9ab732
Felix Ocker <felix.ocker@googlemail.com>
2022-09-16 01:18:03 +0200

    chore: bump version

--- src/_about.py
+++ src/_about.py
@@ -3,7 +3,7 @@
 __name__ = "prom"
 __author__ = "Felix Ocker"
 __author_email__ = "felix.ocker@googlemail.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __url__ = "https://github.com/felixocker/prom"
 __download_url__ = "https://github.com/felixocker/prom/archive/refs/tags/v" + __version__ + ".tar.gz"
 __keywords__ = ["ontology", "owl", "python", "knowledge graph", "ontology merging", "ontology matching"]
